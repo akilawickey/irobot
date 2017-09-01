9ee58cf6d34201f3106a1261687bca2b429dd6b8
akila <asak999@gmail.com>
2017-09-01 06:00:17 +0000

    color detection

---
+++ cam_test.py
@@ -0,0 +1,21 @@
+import cv2
+import numpy as np
+
+# create video capture
+cap = cv2.VideoCapture(0)
+
+# Loop to continuously get images
+while(1):
+    # Read the frames frome a camera
+    _,frame = cap.read()
+
+    # show image
+    cv2.imshow('frame', frame)
+
+    # if key pressed is 'Esc' then exit the loop
+    if cv2.waitKey(33)== 27:
+        break
+    
+# Clean up and exit the program
+cv2.destroyAllWindows()
+cap.release()
